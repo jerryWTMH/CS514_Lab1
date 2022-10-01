3e8dfe70de778115d8ab8f7ff54d657bb1c73f94
Jerry Hsu <jerry1202.wtmh@gmail.com>
2022-10-01 15:59:47 -0400

    task2

--- Task2/CustomAbr.py
+++ Task2/CustomAbr.py
@@ -4,14 +4,16 @@ class CustomAbr(sabre.Abr):
     def __init__(self, config):
         super().__init__(config)
         # Your variables here:
+        self.myQ = 0
 
     def get_quality_delay(self, segment_index):
         manifest = self.session.manifest
         bitrates = manifest.bitrates
         throughput = self.session.get_throughput()
         buffers = self.session.get_buffer_contents()
-        quality = 0
-        while (quality + 1 < len(bitrates) and
+        quality = self.myQ
+        while (quality + 1 < len(bitrates) and abs(quality - self.myQ) < 2 and 
                bitrates[quality + 1] <= throughput):
             quality += 1
+        self.MQ = quality
         return (quality, 0)
