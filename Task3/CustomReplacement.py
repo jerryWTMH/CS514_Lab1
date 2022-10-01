8b1ecf987a7cf57cee097e3c8e2da6bb47675c34
Jerry Hsu <jerry1202.wtmh@gmail.com>
2022-10-01 16:20:30 -0400

    task3

--- Task3/CustomReplacement.py
+++ Task3/CustomReplacement.py
@@ -3,10 +3,19 @@ import sabre
 
 class CustomReplacement(sabre.Replacement):
     def check_replace(self, quality):
+        manifest = self.session.manifest
+        bitrates = manifest.bitrates
+        throughput = self.session.get_throughput()
         buffer = self.session.get_buffer_contents()
+        available = 0
+        for index, bitrate in enumerate(bitrates):
+            if bitrate <= throughput:
+                available = index
+            else:
+                break
         for i in range(2, len(buffer)):
-            if buffer[i] < quality:
+            if buffer[i] < available:
                 # return -ve index from end of buffer
-                return (i - len(buffer), 0)
+                return (i - len(buffer), available)
         # if we arrive here, no switching occurs
         return (None, 0)
